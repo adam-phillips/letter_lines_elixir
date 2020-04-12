f17dd22040128b5cd8b61037f59665f095fbcf10
Adam Phillips <aadam21@gmail.com>
2020-04-11 21:44:12 -0400

    Add Letter Checking to Board State
    Add the ability to a BoardState to check a given coordinate for a letter that
    is part of a word. Return the letter if found, a message if not, or raise
    if multiple, different letters are found which indicates a faulty board state

--- lib/letter_lines_elixir/board_state.ex
+++ lib/letter_lines_elixir/board_state.ex
@@ -13,10 +13,30 @@ defmodule LetterLinesElixir.BoardState do
   def new(words) do
     {max_x, max_y} = BoardWord.get_max_size(words)
 
+    for x <- 0..(max_x - 1), y <- 0..(max_y - 1) do
+      _ = do_get_letter_at(words, x, y)
+    end
+
     %BoardState{
       width: max_x,
       height: max_y,
       words: words
     }
+  end
+
+  def get_letter_at(%BoardState{words: words}, x, y) do
+    do_get_letter_at(words, x, y)
+  end
+
+  defp do_get_letter_at(words, x, y) do
+    words
+    |> Enum.map(&BoardWord.get_letter_at(&1, x, y))
+    |> Enum.reject(&(&1 == :none))
+    |> Enum.uniq()
+    |> case do
+         [] -> :none
+         [a] -> a
+         [a | _] = list -> raise "Multiple letters found at #{x}, #{y}: #{inspect(list)}"
+       end
   end
 end
