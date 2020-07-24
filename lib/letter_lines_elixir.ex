cae419c5fc23e68bb0c67deb40cdf20256478523
Adam Phillips <aadam21@gmail.com>
2020-07-24 15:07:34 -0400

    Add game initialization (#25)

--- lib/letter_lines_elixir.ex
+++ lib/letter_lines_elixir.ex
@@ -2,17 +2,20 @@ defmodule LetterLinesElixir do
   @moduledoc """
   Documentation for LetterLinesElixir.
   """
+  alias LetterLinesElixir.BoardState
+  alias LetterLinesElixir.BoardWord
 
-  @doc """
-  Hello world.
+  def generate_game() do
+    words = [
+      BoardWord.new(5, 0, :v, "bunch"),
+      BoardWord.new(0, 1, :v, "chub"),
+      BoardWord.new(3, 1, :v, "burn"),
+      BoardWord.new(0, 1, :h, "curb"),
+      BoardWord.new(0, 4, :h, "brunch")
+    ]
 
-  ## Examples
+    board_state = BoardState.new(words)
 
-      iex> LetterLinesElixir.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    LetterLinesElixir.GameState.new(board_state)
   end
 end
